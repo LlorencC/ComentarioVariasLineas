9e5df830e584cdadc91679989e1668ad506abee5
LlorencC <llorenc.carricondo@gmail.com>
2022-09-10 04:18:33 +0200

    ComentarioVariasLineas prueba

---
+++ ComentarioVariasLineas/src/comentarioVariasLineas/ComentarioVariasLineas.java
@@ -0,0 +1,16 @@
+package comentarioVariasLineas;
+
+public class ComentarioVariasLineas {
+
+	public static void main(String[] args) {
+		/*System.out.println("Hola");
+		System.out.println("Adiós);
+		System.out.print("Cucu");
+		 */
+		for (int i=0;i<6;/*ahora viene el paso*/i++) {
+			System.out.print(i+" ");
+		}
+		System.out.print("FIN DEL PROGRAMA");
+	}
+
+}
